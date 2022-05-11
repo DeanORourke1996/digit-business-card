a10823b1825d2c9efca686ef8e87f0c9d6f1b1fb
DeanORourke1996 <dean.orourke@hotmail.com>
2022-05-11 17:23:47 +0100

    Adding footer section and footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,40 @@
+// Core
 import React from "react";
 
+// Custom
+import styles from "./Footer.module.css";
+import twitterLogo from "../images/Twitter-logo.png";
+import githubLogo from "../images/Github-logo.png";
+
 export default function Footer() {
-    return(<h2>This is the Footer section.</h2>);
+    return(
+        <footer className={styles.footer}>
+            <div className={styles.socialMedia}>
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={styles.links}
+                    href="https://github.com/DeanORourke1996"
+                >
+                    <img 
+                        src={githubLogo}
+                        alt="Click to go to my Github"
+                        className={styles.socialIcons}
+                    />
+                </a>
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={styles.links}
+                    href="https://twitter.com/Dean0r96"
+                >
+                    <img
+                        src={twitterLogo}
+                        alt="Click to go to my Twitter"
+                        className={styles.socialIcons}
+                    />
+                </a>
+            </div>
+        </footer>
+    );
 }
