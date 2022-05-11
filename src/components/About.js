6109c8a6c5e148c95327928fa2b8973668fbe090
DeanORourke1996 <dean.orourke@hotmail.com>
2022-05-11 16:55:53 +0100

    Adding about section and moduled css for the about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,19 @@
+// Core
 import React from "react";
 
+// Custom
+import styles from "./About.module.css";
+
 export default function About() {
-    return(<h2>This is the About section.</h2>);
+    return(
+        <main className={styles.about}>
+            <h2 className={styles.header}></h2>
+            <p className={styles.text}>
+                Experience working as a full stack web developer for an
+                Irish travel agency in Dublin. Good software is made to
+                be accessible by all. Love to create unique user
+                interfaces with excellent navigation.
+            </p>
+        </main>
+    );
 }
